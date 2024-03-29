fe0a2f1c19831c3b8fa5846d827eba308bf5cac6
Jorge Alvarado <jalvaradoas39@outlook.com>
2024-03-29 13:50:58 -0400

    Setup MySql database

---
+++ prisma/migrations/20240329174016_add/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `quantity` INTEGER NOT NULL DEFAULT 0,
+    `imageUrl` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

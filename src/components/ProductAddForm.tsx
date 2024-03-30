c43688a99519695807dc6db0907ba6d185408b15
Jorge Alvarado <jalvaradoas39@outlook.com>
2024-03-30 13:34:14 -0400

    add image functionality

--- src/components/ProductAddForm.tsx
+++ src/components/ProductAddForm.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import { IconButton } from '@mui/material';
 import {
   Box,
   Button,
@@ -13,7 +16,46 @@ import {
 } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
+interface Product {
+  name: string;
+  price: string;
+  quantity: number;
+  description: string;
+  image: File | null;
+}
+
 export default function ProductAddForm() {
+  const [fields, setFields] = React.useState<Product>({
+    name: '',
+    price: '',
+    quantity: 0,
+    description: '',
+    image: null,
+  });
+
+  const [imagePreview, setImagePreview] = React.useState<string | null>('');
+
+  const handleImageChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const files = evt.target.files;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+
+      setFields((prevFields) => ({
+        ...prevFields,
+        image: file,
+      }));
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          setImagePreview(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <>
       <form>
@@ -71,6 +113,9 @@ export default function ProductAddForm() {
                 type="file"
                 accept="image/*"
                 required
+                id="image"
+                name="image"
+                onChange={handleImageChange}
                 style={{ display: 'none' }}
               />
               <Box
@@ -114,6 +159,61 @@ export default function ProductAddForm() {
                 </Typography>
               </Box>
             </label>
+
+            <Box>
+              {imagePreview && (
+                <Box
+                  sx={{
+                    position: 'relative',
+                    mt: 2,
+                    display: 'inline-flex',
+                    flexDirection: 'column',
+                    overflow: 'hidden',
+                    borderRadius: '0.375rem',
+                    marginRight: '0.5rem',
+                    border: '1px solid #E5E5E5',
+                  }}
+                >
+                  <Box
+                    sx={{
+                      position: 'relative',
+                      height: '4rem',
+                      width: '7rem',
+                      borderRadius: '0.375rem',
+                      overflow: 'hidden',
+                    }}
+                  >
+                    <Image
+                      src={imagePreview}
+                      alt="Apples"
+                      loading="lazy"
+                      layout="fill"
+                      style={{ objectFit: 'contain' }}
+                    />
+                  </Box>
+                  <IconButton
+                    sx={{
+                      position: 'absolute',
+                      top: '0.25rem',
+                      display: 'flex',
+                      height: '1rem',
+                      width: '1rem',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      borderRadius: '9999px',
+                      backgroundColor: '#DC2626',
+                      color: '#FFFFFF',
+                      fontSize: '0.625rem',
+                      outline: 'none',
+                      right: '0.25rem',
+                    }}
+                    onClick={() => setImagePreview(null)}
+                  >
+                    x
+                  </IconButton>
+                </Box>
+              )}
+            </Box>
           </Box>
         </Box>
 
